37d43b8a62f59cd5fbfacd6d26a1ce1713d8ddc2
Break Yang <breakds@gmail.com>
2023-02-24 15:45:45 -0800

    Add central config

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # traintrack
 Manages and schedules the training jobs on a cluster of machines.
+
+
+## Development Notes
+
+1. To run the server which is built with [FastAPI](https://fastapi.tiangolo.com/)
+   - Development time: `uvicorn traintrack.agent:app --reload`
+   - Standalone binary: `python -m traintrack.agent`
--- traintrack/central.py
+++ traintrack/central.py
@@ -1,6 +1,7 @@
 import os
 import socket
 
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
@@ -27,3 +28,16 @@ async def on_startup():
         raise ValueError("TRAINTRACK_CENTRAL_CONFIG environment variable is not set")
 
     logger.success(f"Traintrack central started on {socket.gethostname()}.")
+
+
+def main():
+    port = os.environ.get("TRAINTRACK_CENTRAL_PORT")
+    if port is None:
+        port = 5976
+    else:
+        port = int(port)
+    uvicorn.run(app, host="127.0.0.1", port=port, log_level="info")
+
+
+if __name__ == "__main__":
+    main()
